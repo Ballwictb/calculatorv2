938f5af1fb1cfd15f371b05a41fe4217babcc0e3
Ballwictb <ballwictb@gmail.com>
2024-03-12 05:57:22 +0100

    🔵

---
+++ src/js/script-calc.js
@@ -0,0 +1,22 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function clearNumber() {
+    var display = document.getElementById('display');
+    var valor = display.value;
+    display.value = valor.slice(0, -1);
+
+}
